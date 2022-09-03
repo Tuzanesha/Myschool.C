d04bc71f542668b5dc8b2d77ec4bbb5ac41caab1
Tuzanesha <cyerekezodany1@gmail.com>
2022-09-03 22:27:31 +0200

    Myschool.c Third Commit

--- Myschool.c
+++ Myschool.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-  printf ("There was an old man named Daniel"); 
+int main ()
+{
+  printf ("There was an old man named Daniel" ); 
+  printf ("Hello world!\n");
+  printf ("There was a lady named Irankunda");
+
   return 0;  
 }
